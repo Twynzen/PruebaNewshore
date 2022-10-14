722b8fdbc8781b2b85e06217dd95441867f62657
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2022-10-13 20:34:46 -0500

    feat: finalización de prueba newshore

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Para ejecutar el proyecto acceda a esta url:
+https://twynzen.github.io/PruebaNewshore/
+
 # División del proyecto
 Esta dividido en 3 carpetas principales
 1. Interfaces
@@ -5,7 +8,20 @@ Esta dividido en 3 carpetas principales
 2. Modules
 > Donde tenemos los componentes principales a usar
 3. Services
-> Donde tenemos la logica que realiza las peticiones http
+> Donde tenemos la lógica que realiza las peticiones http
 
-# Para ejecutar el proyecto sencillamente acceda a esta url:
-https://twynzen.github.io/PruebaNewshore/
+# Comentarios varios 
+* No fue necesario utilizar más que un componente/clase para realizar la prueba, la lógica principal está en el componente flight
+* La función encargada de obtener los datos en el componente es muy extensa y es necesario dividirla de una forma más organizada, pues debería ser más que una sola función. 
+
+```js 
+ getFlights()
+```
+* La lógica en cuanto las escalas requiere de más pruebas para encontrar posibles fallos, además esta en desorden la forma en que se muestran al realizar la consulta
+
+# Detalles a revisar por falta de tiempo, no dispuse de las 72 horas exactas suministradas para realizar la prueba
+* Es necesario aplicar pruebas unitarias
+* Es necesario aplicar interceptores
+* No se realizó el problema 4 de cambio de moneda
+* Es necesario aplicar aplicaron estilos
+* Es necesario aplicar ordenamiento de funciones y buenas practicas, el código fue con algo de afán, por temas responsabilidades externas a la prueba y necesita una revisión de clean code
--- src/app/modules/flight/flight.component.ts
+++ src/app/modules/flight/flight.component.ts
@@ -19,6 +19,12 @@ export class FlightComponent implements OnInit {
   destination: string = '';
   price: number = 100;
   match: any;
+  matched: boolean = false;
+  firstConsult: boolean = true;
+  scales: boolean = false;
+  departure: any;
+  arrival: any;
+  scaleMatch: any;
 
 
 
@@ -29,7 +35,8 @@ export class FlightComponent implements OnInit {
 
   journeyQuestions = this.formBuilder.group({
     origin: ['', [Validators.required, Validators.maxLength(3)]],
-    destination: ['', [Validators.required, Validators.maxLength(3)]]
+    destination: ['', [Validators.required, Validators.maxLength(3)]],
+    typeRoute: [null, [Validators.required]]
   })
 
 
@@ -38,27 +45,70 @@ export class FlightComponent implements OnInit {
   }
 
   getFlights() {
-    this.RestService.get('https://recruiting-api.newshore.es/api/flights/1').subscribe(res => {
-      console.log(res);
 
+    this.RestService.get('https://recruiting-api.newshore.es/api/flights/' + this.journeyQuestions.value.typeRoute).subscribe(res => {
+      console.log(res);
       let uniqueRoutes: any = res;
+      //sin escalas
       this.match = uniqueRoutes.filter((item: { departureStation: string; arrivalStation: string; }) =>
         (item.arrivalStation === this.journeyQuestions.value.destination)
         && (item.departureStation === this.journeyQuestions.value.origin));
-        if (this.match.length === 1) {
-          console.log(this.match);
-        }else{
-          console.log('No se ha encontrado el vuelo sugerido');
 
-        }
+      if (this.match.length === 1) {
+        console.log(this.match);
+        this.matched = true;
+        this.scales = false;
+      } else if (this.match.length !== 1) {
+        //Con escalas
+        this.departure = uniqueRoutes.filter((item: { departureStation: string; }) =>
+          (item.departureStation === this.journeyQuestions.value.origin));
+        console.log("dep", this.departure);
+        this.arrival = uniqueRoutes.filter((item: { arrivalStation: string; }) =>
+          (item.arrivalStation === this.journeyQuestions.value.destination));
+        console.log("ARR", this.arrival);
+        this.departure.forEach((elementDeparture: any) => {
+          this.arrival.forEach((elementArrival: any) => {
 
+            if ((elementDeparture.departureStation === this.journeyQuestions.value.origin)
+              && (elementArrival.arrivalStation === this.journeyQuestions.value.destination)) {
+
+              this.scaleMatch = uniqueRoutes.filter((item: { departureStation: string; arrivalStation: string; }) =>
+                (item.arrivalStation === elementArrival.departureStation)
+                && (item.departureStation === elementDeparture.arrivalStation));
+
+              if (elementArrival.departureStation === this.scaleMatch[0].arrivalStation) {
+                this.scaleMatch.push(elementArrival);
+              }
+              if (elementDeparture.arrivalStation === this.scaleMatch[0].departureStation) {
+                this.scaleMatch.push(elementDeparture);
+              }
+              console.log(this.scaleMatch);
+
+
+              if (this.scaleMatch.length === 0) {
+                this.matched = false;
+                this.scales = false;
+              } else {
+                this.scaleMatch.forEach((element: any) =>{
+                  this.price += element.price;
+                })
+                this.scales = true;
+              }
+
+            }
+          })
+        });
+      } else {
+        this.scales = false;
+        console.log('Su consulta no puede ser procesada.');
+        this.matched = false;
+        this.firstConsult = false;
+      }
 
     })
   }
   sendQuestion() {
     this.getFlights();
-    console.log(this.journeyQuestions.value, "HI good morning");
-
   }
 
 }
